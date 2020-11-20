808c4b71cd0491f0c7cab15f1850633ad8a2fe40
mdabuhasnat <36214690+mdabuhasnat@users.noreply.github.com>
2020-11-20 12:11:07 +0600

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -4,13 +4,9 @@ package in.javahome.myweb.controller;
  */
 public class Calculator {
 	
-	/*
-	 * @param i
-	 * @param j
-	 * @return int
-	 */
-	int i=3;
-	int j=2;
+	public int i=3;
+	public int j=2;
+	
 	public int add(int i, int j){
 		String name = null;
 		name.toUpperCase();
